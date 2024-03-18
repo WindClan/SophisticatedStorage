46b508b27ac9dd96c2c139bf2518c5e594a64552
Salandora <salandora@gmail.com>
2024-03-18 18:46:23 +0100

    [Improvement]: Cherry picked and combined all build and github action changes

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/mixin/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Implementation code for SophisticatedStorage.
+ */
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedstorage.mixin;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
