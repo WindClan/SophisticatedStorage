cb6ea305346abea316bb84975de24d7933dd03f4
Salandora <salandora@gmail.com>
2024-03-21 22:46:19 +0100

    [Fix]: Game crashed when emi was not loaded

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/emi/EmiCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/emi/EmiCompat.java
@@ -3,15 +3,21 @@ package net.p3pp3rf1y.sophisticatedstorage.compat.emi;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiCraftingRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import dev.emi.emi.api.stack.Comparison;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.Bounds;
 
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.level.block.Block;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.SettingsScreen;
 import net.p3pp3rf1y.sophisticatedcore.compat.emi.EmiGridMenuInfo;
 import net.p3pp3rf1y.sophisticatedcore.compat.emi.EmiSettingsGhostDragDropHandler;
@@ -29,10 +35,25 @@ import net.p3pp3rf1y.sophisticatedstorage.item.BarrelBlockItem;
 import net.p3pp3rf1y.sophisticatedstorage.item.StorageBlockItem;
 import net.p3pp3rf1y.sophisticatedstorage.item.WoodStorageBlockItem;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class EmiCompat implements EmiPlugin {
-    @Override
+	public static Event<WorkstationCallback> WORKSTATIONS = EventFactory.createArrayBacked(WorkstationCallback.class, (listeners) -> (consumer) -> {
+		for (WorkstationCallback listener : listeners) {
+			listener.additionalWorkstations(consumer);
+		}
+	});
+
+	public record WorkstationEntry(ResourceLocation id, Block icon, Item workstation) {}
+
+	public interface WorkstationCallback {
+		void additionalWorkstations(Consumer<WorkstationEntry> consumer);
+	}
+
+	@Override
     public void register(EmiRegistry registry) {
         registry.addExclusionArea(StorageScreen.class, (screen, consumer) -> {
             screen.getUpgradeSlotsRectangle().ifPresent(r -> consumer.accept(new Bounds(r.getX(), r.getY(), r.getWidth(), r.getHeight())));
@@ -125,6 +146,12 @@ public class EmiCompat implements EmiPlugin {
 
 		registry.addWorkstation(VanillaEmiRecipeCategories.CRAFTING, EmiStack.of(ModItems.CRAFTING_UPGRADE));
 		registry.addWorkstation(VanillaEmiRecipeCategories.STONECUTTING, EmiStack.of(ModItems.STONECUTTER_UPGRADE));
+
+		List<WorkstationEntry> entries = new ArrayList<>();
+		WORKSTATIONS.invoker().additionalWorkstations(entries::add);
+		for (WorkstationEntry entry : entries) {
+			registry.addWorkstation(new EmiRecipeCategory(entry.id, EmiStack.of(entry.icon)), EmiStack.of(entry.workstation));
+		}
     }
 
     private static void registerCraftingRecipes(EmiRegistry registry, Collection<CraftingRecipe> recipes) {
