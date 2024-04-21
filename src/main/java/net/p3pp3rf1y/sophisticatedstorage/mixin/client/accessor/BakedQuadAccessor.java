d2237aaf08fcd646eab5ce5fe49124cfcf303a6f
Salandora <salandora@gmail.com>
2024-04-21 17:17:42 +0200

    [Fix]: Make accessors mutable

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/mixin/client/accessor/BakedQuadAccessor.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/mixin/client/accessor/BakedQuadAccessor.java
@@ -1,6 +1,7 @@
 package net.p3pp3rf1y.sophisticatedstorage.mixin.client.accessor;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -8,12 +9,14 @@ import net.minecraft.core.Direction;
 
 @Mixin(BakedQuad.class)
 public interface BakedQuadAccessor {
+	@Mutable
 	@Accessor
 	void setDirection(Direction direction);
 
 	@Accessor
 	int getTintIndex();
 
+	@Mutable
 	@Accessor
 	void setTintIndex(int tintIndex);
 }
