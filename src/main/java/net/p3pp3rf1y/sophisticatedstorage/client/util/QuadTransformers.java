0a18b4278bc41dc68ad51ca64a7ff98494472f55
Salandora <salandora@gmail.com>
2024-05-27 00:10:39 +0200

    [Fix]: QuadTransformers causing a crash when the BakedQuad has it's isShaded flag set to true

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/util/QuadTransformers.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/util/QuadTransformers.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuadTransformers {
-	private static MutableQuadViewImpl getEditorQuad() {
-		return new MutableQuadViewImpl() {
+	private static MutableQuadViewImpl getEditorQuad(BakedQuad quad) {
+		MutableQuadViewImpl mqv = new MutableQuadViewImpl() {
 			{
 				this.data = new int[EncodingFormat.TOTAL_STRIDE];
 			}
@@ -24,6 +24,13 @@ public class QuadTransformers {
 				// noop
 			}
 		};
+
+		mqv.fromVanilla(quad.getVertices(), 0);
+		mqv.nominalFace(quad.getDirection());
+		mqv.colorIndex(quad.getTintIndex());
+		mqv.tag(0);
+
+		return mqv;
 	}
 
 	public static List<BakedQuad> process(RenderContext.QuadTransform transform, List<BakedQuad> quads) {
@@ -31,7 +38,7 @@ public class QuadTransformers {
 
 		for (int i = 0; i < quads.size(); i++) {
 			BakedQuad quad = quads.get(i);
-			MutableQuadView mqv = getEditorQuad().fromVanilla(quad, null, null);
+			MutableQuadView mqv = getEditorQuad(quad);
 			transform.transform(mqv);
 
 			BakedQuad transformedQuad = mqv.toBakedQuad(quad.getSprite());
