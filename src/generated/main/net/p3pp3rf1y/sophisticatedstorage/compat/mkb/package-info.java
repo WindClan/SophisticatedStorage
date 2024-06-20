0df67686dd06f4cc2313cbf604570bd7040c56d5
Salandora <salandora@gmail.com>
2024-06-20 22:44:20 +0200

    [General]: Added package-info for mkb compat

---
+++ src/generated/main/net/p3pp3rf1y/sophisticatedstorage/compat/mkb/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Implementation code for SophisticatedStorage.
+ */
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedstorage.compat.mkb;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
